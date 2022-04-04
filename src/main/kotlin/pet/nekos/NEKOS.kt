4f80ee3fdbb82376ecd96f8eb7367ae9a78b123e
diademiemi <emilia@diademiemi.me>
2022-04-04 15:53:28 +0200

    Change testing NEKOS

--- src/main/kotlin/pet/nekos/NEKOS.kt
+++ src/main/kotlin/pet/nekos/NEKOS.kt
@@ -1,6 +1,10 @@
 package pet.nekos.nekos
+
 import pet.nekos.api.server.Server
 import pet.nekos.api.event.message.MessageEvent
+
+import pet.nekos.api.user.User
+import pet.nekos.api.message.Message
 
 import kotlinx.coroutines.*
 
@@ -17,10 +21,13 @@ object NEKOS {
 
         delay(2000)
         println("Testing firing a message...")
-        server.serverManager.fireEvent(MessageEvent("TEST CONTENT"))
+        var user = User("TestSender", "Sender", "TEMPORARY")
+        var message = Message("Testing Message", user)
+        server.serverManager.fireEvent(MessageEvent(message))
 
+        message = Message("Testing Message 2", user)
         delay(2000)
         println("Testing firing a message... 2 ")
-        server.serverManager.fireEvent(MessageEvent("TEST CONTENT 2"))
+        server.serverManager.fireEvent(MessageEvent(message))
     }
 }
