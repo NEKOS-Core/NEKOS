2c3f0d94be599065a2580949038b753a2e288d45
diademiemi <emilia@diademiemi.me>
2022-04-10 01:47:06 +0200

    Remove builtin tests

--- src/main/kotlin/pet/nekos/NEKOS.kt
+++ src/main/kotlin/pet/nekos/NEKOS.kt
@@ -5,6 +5,7 @@ import pet.nekos.api.event.message.MessageEvent
 
 import pet.nekos.api.user.User
 import pet.nekos.api.message.Message
+import pet.nekos.api.channel.Channel
 
 import kotlinx.coroutines.*
 
@@ -19,15 +20,5 @@ object NEKOS {
         server.loadPlugins()
         server.loadServices()
 
-        delay(2000)
-        println("Testing firing a message...")
-        var user = User("TestSender", "Sender", "TEMPORARY")
-        var message = Message("Testing Message", user)
-        server.serverManager.fireEvent(MessageEvent(message))
-
-        message = Message("Testing Message 2", user)
-        delay(2000)
-        println("Testing firing a message... 2 ")
-        server.serverManager.fireEvent(MessageEvent(message))
     }
 }
